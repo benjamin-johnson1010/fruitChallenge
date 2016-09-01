1b24ed83a541c7e168c54d826ddbb54ea554c7c5
BENJAMIN JOHNSON <benjaminjohnson@BENJAMINs-MacBook-Air.local>
2016-09-01 15:07:41 -0500

    second commit

--- scripts/fruit.js
+++ scripts/fruit.js
@@ -2,20 +2,70 @@ console.log('sourced');
 
 var inventory = [];
 var fruit = {
-  name:'',
-  price:''
+  name: '',
+  price: 0,
 };
 
-$(document).ready(function (){
-  $('#orange').on('click', function(){
-    console.log('clicked orange');
-    createFruit();
+var userMoney = 100;
 
+function randomNum(min, max) {
+  return Number((Math.random() * (max - min) + min).toFixed(2));
+}
+
+var orange = {
+  name: 'orange',
+  price: randomNum(0.5, 9.99),
+};
+
+var apple = {
+  name: 'apple',
+  price: randomNum(0.5, 9.99),
+};
+
+var banana = {
+  name: 'banana',
+  price: randomNum(0.5, 9.99),
+};
+
+var grape = {
+  name: 'grape',
+  price: randomNum(0.5, 9.99),
+};
+
+var arrayOfPrices = [orange, apple, banana, grape];
+
+$(document).ready(function (){
+  setInterval(function(){
+    for (var i = 0; i < arrayOfPrices.length; i++) {
+      if(arrayOfPrices[i].price >= 9.49) {
+        arrayOfPrices[i].price -= randomNum(0.01, 0.5);
+      } else if(arrayOfPrices[i].price <= 1) {
+        arrayOfPrices[i].price += randomNum(0.01, 0.5);
+      } else {
+        arrayOfPrices[i].price += (-0.5, 0.5);
+      }
+    }
+    console.log(arrayOfPrices);
+  }, 15000);
+  console.log(orange);
+
+  $('.buyButton').on('click', function(){
+    console.log('clicked button');
+
+    var fruitName = $(this).data('name');
+    fruit.name = fruitName;
+    // fruit.price = fruitName.price;
+    loopOfPrices();
+    inventory.push(fruit);
+    console.log(inventory);
   });
-  console.log(fruit);
 });
 
-var createFruit = function(){
-  console.log('in createFruit');
-  fruit.name = $(this).id;
+var loopOfPrices = function(){
+  console.log('in loop price');
+  for (var i = 0; i < arrayOfPrices.length; i++) {
+    if(arrayOfPrices[i].name === $('.buyButton').data('name'))
+      fruit.price = arrayOfPrices[i].price;
+      return fruit.price;
+  }
 };
